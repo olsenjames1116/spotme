4122075398f688c34a32f6ef153873fc669f5b10
James Olsen <olsenjames1116@gmail.com>
2024-08-11 14:01:20 -0400

    chore: add SQL statements to create tables for auth

--- server/src/main/resources/init.sql
+++ server/src/main/resources/init.sql
@@ -5,3 +5,21 @@ CREATE USER 'spotme'@'localhost' IDENTIFIED BY 'dinero';
 CREATE DATABASE IF NOT EXISTS `spotme-dev`;
 
 GRANT ALL PRIVILEGES ON `spotme-dev`.* TO 'spotme'@'localhost';
+
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `user` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(68) NOT NULL,
+    enabled TINYINT NOT NULL,
+    balance NUMERIC(6,2)
+);
+
+CREATE TABLE `role` (
+    user_id INT NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    UNIQUE KEY `role_idx_1` (`user_id`, `role`),
+    CONSTRAINT `role_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) 
+);
