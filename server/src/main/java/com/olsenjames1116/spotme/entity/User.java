b40172fbbee5cb4609f0552719894511426aa198
James Olsen <olsenjames1116@gmail.com>
2024-08-11 16:13:55 -0400

    feat: add classes for repository connection

---
+++ server/src/main/java/com/olsenjames1116/spotme/entity/User.java
@@ -0,0 +1,60 @@
+package com.olsenjames1116.spotme.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "balance")
+    private double balance;
+
+    public User() {}
+
+    public User(String username, double balance) {
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username='" + username + '\'' + ", balance=" + balance
+                + '}';
+    }
+}
